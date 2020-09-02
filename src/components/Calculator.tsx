ea7c0e12f613dc46c803753668e379e8e258d0e4
eugenekim000 <eugenekim000@gmail.com>
2020-09-01 20:02:48 -0700

    basic math operations implemented

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,31 +1,59 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 
 interface Props {}
 
 export default function Calculator({}: Props): ReactElement {
+  const [headerNumber, setHeaderNumber] = useState("");
+  const [totalNumber, setTotalNumber] = useState("");
+  const [operator, setOperator] = useState("");
+
+  const handleNumberInput = (numString: string) => {
+    setHeaderNumber((prevState) => prevState + numString);
+  };
+
+  const handleOperationInput = (operator: string) => {
+    const opTable: { [key: string]: (arg1: number, arg2: number) => number } = {
+      "/": (num1: number, num2: number) => num1 / num2,
+      "+": (num1: number, num2: number) => num1 + num2,
+      "-": (num1: number, num2: number) => num1 - num2,
+      "*": (num1: number, num2: number) => num1 * num2,
+    };
+
+    if (operator && totalNumber !== headerNumber) {
+      let number1 = Number(totalNumber);
+      let number2 = Number(headerNumber);
+      let newNumber = opTable[operator](number1, number2).toString();
+      setHeaderNumber(newNumber);
+      setTotalNumber(newNumber);
+      setOperator("");
+    } else {
+      setOperator(operator);
+    }
+  };
+
   return (
     <div className="calculator">
-      <div className="calculator-header">Header</div>
+      <div className="calculator-header">{headerNumber ? headerNumber : 0}</div>
       <div className="calculator-buttons">
         <button></button>
         <button>C</button>
         <button>x</button>
         <button>÷</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
+        <button onClick={() => handleNumberInput("7")}>7</button>
+        <button onClick={() => handleNumberInput("8")}>8</button>
+        <button onClick={() => handleNumberInput("9")}>9</button>
         <button>X</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
+        <button onClick={() => handleNumberInput("4")}>4</button>
+        <button onClick={() => handleNumberInput("5")}>5</button>
+        <button onClick={() => handleNumberInput("6")}>6</button>
         <button>-</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
+        <button onClick={() => handleNumberInput("1")}>1</button>
+        <button onClick={() => handleNumberInput("2")}>2</button>
+        <button onClick={() => handleNumberInput("3")}>3</button>
         <button>+</button>
         <button>+/-</button>
-        <button>0</button>
-        <button>.</button>
+        <button onClick={() => handleNumberInput("0")}>0</button>
+        <button onClick={() => handleNumberInput(".")}>.</button>
         <button>=</button>
       </div>
     </div>
--- src/components/History.tsx
+++ src/components/History.tsx
@@ -3,5 +3,9 @@ import React, { ReactElement } from "react";
 interface Props {}
 
 export default function History({}: Props): ReactElement {
-  return <div className="history">history</div>;
+  return (
+    <div className="history">
+      asdfasdfkasjdflkjasdlkfjalksdjflkasjdfklasdjfkajsdlkfjlaksdjfklsdj
+    </div>
+  );
 }
