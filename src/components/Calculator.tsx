aac53151657867b87f8fc1fd75ef48f1c7715a98
eugenekim000 <eugenekim000@gmail.com>
2020-09-01 21:12:56 -0700

    fixed calculation renderings

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -2,59 +2,103 @@ import React, { ReactElement, useState } from "react";
 
 interface Props {}
 
+const opTable: { [key: string]: (arg1: number, arg2: number) => number } = {
+  "/": (num1: number, num2: number) => num1 / num2,
+  "+": (num1: number, num2: number) => num1 + num2,
+  "-": (num1: number, num2: number) => num1 - num2,
+  x: (num1: number, num2: number) => num1 * num2,
+};
+
 export default function Calculator({}: Props): ReactElement {
   const [headerNumber, setHeaderNumber] = useState("");
-  const [totalNumber, setTotalNumber] = useState("");
-  const [operator, setOperator] = useState("");
+  const [operatorStack, setOperatorStack] = useState<string[]>([]);
+  const [numberStack, setNumberStack] = useState<string[]>([]);
+  const [result, setResult] = useState("");
+  //todo add a history state.
 
   const handleNumberInput = (numString: string) => {
+    setResult("");
     setHeaderNumber((prevState) => prevState + numString);
   };
 
-  const handleOperationInput = (operator: string) => {
-    const opTable: { [key: string]: (arg1: number, arg2: number) => number } = {
-      "/": (num1: number, num2: number) => num1 / num2,
-      "+": (num1: number, num2: number) => num1 + num2,
-      "-": (num1: number, num2: number) => num1 - num2,
-      "*": (num1: number, num2: number) => num1 * num2,
-    };
+  const handleBackspace = () => {
+    setHeaderNumber((prevState) => prevState.slice(0, -1));
+  };
 
-    if (operator && totalNumber !== headerNumber) {
-      let number1 = Number(totalNumber);
+  const handleClear = () => {
+    setHeaderNumber("");
+    setOperatorStack([]);
+    setNumberStack([]);
+    setResult("");
+  };
+
+  const handleInverse = () => {
+    if (headerNumber) {
+      if (headerNumber[0] === "-") {
+        setHeaderNumber((prevState) => prevState.slice(1));
+      } else {
+        setHeaderNumber((prevState) => "-" + prevState);
+      }
+    }
+  };
+
+  const handleOperationInput = (operator: string) => {
+    if (numberStack.length & operatorStack.length) {
+      let number1 = Number(numberStack[0]);
       let number2 = Number(headerNumber);
-      let newNumber = opTable[operator](number1, number2).toString();
-      setHeaderNumber(newNumber);
-      setTotalNumber(newNumber);
-      setOperator("");
+      let newNumber = opTable[operatorStack[0]](number1, number2).toString();
+      setHeaderNumber("");
+      setOperatorStack([operator]);
+      setNumberStack([newNumber]);
+      setResult(newNumber);
     } else {
-      setOperator(operator);
+      setNumberStack([headerNumber]);
+      setOperatorStack([operator]);
+      setHeaderNumber("");
+      setResult(headerNumber);
+      return;
+    }
+  };
+
+  const handleEqual = () => {
+    if (operatorStack.length) {
+      let number1 = Number(numberStack[0]);
+      let number2 = Number(headerNumber);
+      let newNumber = opTable[operatorStack[0]](number1, number2).toString();
+
+      setHeaderNumber(newNumber);
+      setOperatorStack([]);
+      setNumberStack([newNumber]);
+      setResult(newNumber);
     }
   };
 
   return (
     <div className="calculator">
-      <div className="calculator-header">{headerNumber ? headerNumber : 0}</div>
+      <div className="calculator-header">
+        {result ? result : headerNumber ? headerNumber : 0}
+      </div>
       <div className="calculator-buttons">
         <button></button>
-        <button>C</button>
-        <button>x</button>
-        <button>÷</button>
+        <button onClick={() => handleClear()}>C</button>
+        <button onClick={() => handleBackspace()}>x</button>
+        <button onClick={() => handleOperationInput("/")}>÷</button>
         <button onClick={() => handleNumberInput("7")}>7</button>
         <button onClick={() => handleNumberInput("8")}>8</button>
         <button onClick={() => handleNumberInput("9")}>9</button>
-        <button>X</button>
+        <button onClick={() => handleOperationInput("x")}>X</button>
         <button onClick={() => handleNumberInput("4")}>4</button>
         <button onClick={() => handleNumberInput("5")}>5</button>
         <button onClick={() => handleNumberInput("6")}>6</button>
-        <button>-</button>
+        <button onClick={() => handleOperationInput("-")}>-</button>
         <button onClick={() => handleNumberInput("1")}>1</button>
         <button onClick={() => handleNumberInput("2")}>2</button>
         <button onClick={() => handleNumberInput("3")}>3</button>
-        <button>+</button>
-        <button>+/-</button>
+        <button onClick={() => handleOperationInput("+")}>+</button>
+        <button onClick={() => handleInverse()}>+/-</button>
         <button onClick={() => handleNumberInput("0")}>0</button>
         <button onClick={() => handleNumberInput(".")}>.</button>
-        <button>=</button>
+        <button onClick={() => handleEqual()}>=</button>
       </div>
     </div>
   );
